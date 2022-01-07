a85e166c0696ef55473915429feba15478c36129
Kizmari <kizmari@yandex.ru>
2022-01-07 18:42:30 +0300

    Lesson 3 No Error. Have error:cannot resolve class android.support.design.widget.FloatingActionButton

--- app/src/main/java/com/itproger/chatapp/MainActivity.java
+++ app/src/main/java/com/itproger/chatapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.itproger.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
